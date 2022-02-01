e3761d62b92c4d85550b7a972e6182018a797b61
marlenemora <mjm598@georgetown.edu>
2022-01-31 20:06:42 -0500

    Works up until getting the timer started

---
+++ app/src/main/java/com/example/findbart/FindImage.kt
@@ -0,0 +1,54 @@
+package com.example.findbart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
+
+class FindImage : AppCompatActivity() {
+    lateinit var countDownTimer: CountDownTimer
+    var counterActive = false
+    lateinit var timerView : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_image)
+
+        timerView = findViewById(R.id.timerView)
+
+        startTimer()
+
+    }//onCreate
+
+
+    fun startTimer (view : View) {
+        if (!counterActive) {
+            counterActive = true
+
+            countDownTimer = object : CountDownTimer(1500, 1000) {
+                override fun onTick(p0: Long) {
+                    updateTimer((p0 / 1000).toInt())
+                }
+
+                override fun onFinish() {
+                    //TODO("Not yet implemented")
+                }
+            }// countDownTimer
+            countDownTimer.start()
+        }//startTimer
+        else {
+            countDownTimer.cancel()
+            counterActive = false
+        }
+    }//startTimer
+    fun updateTimer(secondsLeft : Int) {
+        val minutes = secondsLeft / 60
+        val seconds = secondsLeft - minutes * 60
+        var secondString = seconds.toString()
+        if (seconds <= 9) {
+            secondString = "0$secondString"
+        }
+        timerView.text =  minutes.toString()+":" + secondString
+    }//updateTimer
+}//FindImage
--- app/src/main/java/com/example/findbart/MainActivity.kt
+++ app/src/main/java/com/example/findbart/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.findbart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+    }//onCreate
+    fun playGame (view : View) {
+        val intent = Intent(this, FindImage::class.java)
+        startActivity(intent)
+    }//playGame
+}//MainActivity
