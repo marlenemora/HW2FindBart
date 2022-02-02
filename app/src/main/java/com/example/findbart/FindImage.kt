3ab12db06a0129861660706260b54baaad4fff2e
marlenemora <mjm598@georgetown.edu>
2022-02-01 20:34:55 -0500

    FINISHED by adding grid and timer and stuff

--- app/src/main/java/com/example/findbart/FindImage.kt
+++ app/src/main/java/com/example/findbart/FindImage.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class FindImage : AppCompatActivity() {
     lateinit var countDownTimer: CountDownTimer
     var counterActive = false
     lateinit var timerView : TextView
+    var firstGame = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,29 +20,33 @@ class FindImage : AppCompatActivity() {
 
         timerView = findViewById(R.id.timerView)
 
-        startTimer()
+       if (firstGame) {
+           startTimer()
+       }//if
+        else {
+           countDownTimer.cancel()
+
+       }
 
     }//onCreate
-
 
     fun startTimer () {
         if (!counterActive) {
             counterActive = true
 
-            countDownTimer = object : CountDownTimer(16000, 1000) {
+            countDownTimer = object : CountDownTimer(11000, 1000) {
                 override fun onTick(p0: Long) {
                     updateTimer((p0 / 1000).toInt())
                 }
 
                 override fun onFinish() {
-                    //TODO("Not yet implemented")
+                    resetTime()
                 }
             }// countDownTimer
             countDownTimer.start()
         }//startTimer
         else {
             countDownTimer.cancel()
-            counterActive = false
         }
     }//startTimer
     fun updateTimer(secondsLeft : Int) {
@@ -50,5 +57,43 @@ class FindImage : AppCompatActivity() {
             secondString = "0$secondString"
         }
         timerView.text =  minutes.toString()+":" + secondString
+        failed()
     }//updateTimer
+
+    fun wrong (view: View) {
+        Toast.makeText(applicationContext, "WRONG", Toast.LENGTH_SHORT).show()
+    }//wrong
+    fun correct (view : View) {
+        //Toast.makeText(applicationContext, "CORRECT", Toast.LENGTH_SHORT).show()
+        firstGame = false
+        countDownTimer.cancel()
+
+        resetTime()
+
+        val alertDialog = AlertDialog.Builder(this, R.style.AlertDialogStyle)
+        alertDialog.apply {
+            setTitle("CONGRATS! You found Bart!")
+        }.create().show()
+
+    }//correct
+
+    fun resetTime(){
+        counterActive = false
+        timerView.text = "0:10"
+    }//resetTime
+
+    fun playAgain(view: View){
+        counterActive = false
+        startTimer()
+    }//playAgain
+
+    fun failed () {
+        if (timerView.text == "0:00") {
+            val alertDialog = AlertDialog.Builder(this, R.style.AlertDialogStyle)
+            alertDialog.apply {
+                setTitle("LOSER, WHY CAN'T YOU FIND BART?! Try Again!")
+            }.create().show()
+        }
+
+    }//failed
 }//FindImage
